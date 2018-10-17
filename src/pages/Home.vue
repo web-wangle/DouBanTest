d0a3a267502790a20fc6a041b143dffc0338530f
web-wangle <18829291736@163.com>
2018-10-17 16:31:00 +0800

    豆瓣vue项目初始化

---
+++ src/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="home">
+    我是豆瓣主页
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+ data() {
+   return {
+   };
+ },
+};
+</script>
+
+<style scoped>
+</style>
